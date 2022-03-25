c3aff1acd194489327c7580ef23c18c270f1c658
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-03-25 12:52:07 -0300

    configurando a variavel de ambiente da porta na qual o app sera executado

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ import './websocket.js';
 
 const PORT = process.env.PORT || 3000; // A variavel de ambiente "PORT" é definida pelo servidor de hospedagem
 
-serverHttp.listen(3000, ()=>{
-    console.log("Server is running on PORT 3000");
+serverHttp.listen(PORT, ()=>{
+    console.log(`Server is running on PORT ${PORT}`);
 });
 
 //https://www.bing.com/videos/search?q=jogos+para+fazer+com+websocket&&view=detail&mid=9C6C622132B882879CAB9C6C622132B882879CAB&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Djogos%2Bpara%2Bfazer%2Bcom%2Bwebsocket%26FORM%3DHDRSC3
